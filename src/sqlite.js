5a4c2c395c036821854a83c62a92e92e0a895458
AlirezaAkbarpour <akbarpouralireza1>
2024-07-24 16:55:30 +0330

    database handle

--- src/sqlite.js
+++ src/sqlite.js
@@ -1,4 +1,4 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('data',sqlite3.OPEN_READWRITE,(error)=>{
+const db = new sqlite3.Database('./data.db',sqlite3.OPEN_READWRITE,(error)=>{
     if(error) console.error(error)
 })
